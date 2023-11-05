b78eb8c99b6980350b24912899be2beb0098c57f
Matias Slimmens <65687933+mslimmens@users.noreply.github.com>
2023-11-04 21:29:36 -0300

    Add files via upload

---
+++ carta/index.php
@@ -0,0 +1,157 @@
+<!--head-->
+<?php include("./layout/main.php") ?> 
+<body id="home" class="bg-dark-color">
+    <!--header-->
+    <?php include("./layout/header.php") ?> 
+    <!--limpiando codigo-->
+    <!--main-->
+    <main>
+    <div class="main-banner bg-dark-color">
+        <div class="d-table">
+            <div class="d-table-cell">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-8 col-md-12">
+                            <div class="main-banner-content white-color">
+                                <!--<span>Bienvenidos a <b>Bitácora Angelical</b></span>-->
+                                <h2>Bienvenidos a <b>Bitácora Angelical</b></h2 >
+
+                                <div class="title">
+                                    <h1>Tu punto de conexión.</h1>
+                                    
+                                </div>
+                                <h4> <b> Magali Baldovino </b></h4>
+                                <p>  Angeologa Internacional | Terapeuta Angelica | Lectora de Registros akashicos.</p>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-12">
+                            <div class="banner-image" style="position:relative">
+                                    <img src="img/pp.jpg" class="rounded-circle">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </main>
+    <?php include("./layout/learning.php") ?>
+    <!--<div class="main-banner bg-dark-color">
+        <div class="d-table">
+            <div class="d-table-cell">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-md-12">
+                            <div class="main-banner-content white-color">
+                                <span><b>Novedades, Terapias y Talleres</b></span>
+                                <div id="accordion">
+                                    <button class="accordion"><p>Sanación con ángeles</p></button>
+                                    <div>
+                                    <p>
+                                        La terapia con ángeles es un proceso de guía y sanación que trabaja con ángeles y arcángeles quienes asisten para que la persona pueda, perdonar, sanar y liberar situaciones o emociones que le han bloqueado en su vida y que le han impedido disfrutar de la felicidad. <br> 
+                                        Estos bloqueos energéticos darán origen a una enfermedad o el estancamiento en diferentes áreas de nuestra vida; cuando no fluye la energía vital ésta se manifestará de alguna forma en nuestra realidad. <br>
+                                        La sanación angelical trae una poderosa y maravillosa armonía para quien la recibe directamente y para las personas que le rodean, pues se benefician de la nueva energía de amor y paz que la persona transmite y genera a su alrededor. <br>
+                                        Es un proceso espiritual, amoroso y muy sanador que nos ayuda a regresar a nuestra esencia Divina para sanarnos total e integralmente (Mente-Cuerpo-Espíritu).
+                                    </p>
+                                    </div>
+                                    <button class="accordion"><p>Carta natal angelical</p></button>
+                                    <div>
+                                        <p>
+                                        ¿Queres conocer que ángeles te acompañan? ¿Te gustaría Saber cuales son sus cualidades y como pueden ayudarte en tu vida? <br>
+                                        Te invito a que solicites tu Carta angelical y puedas conectar con la maravillosa energía de tus ángeles. <br>
+                                        La carta angelical te indica según tu fecha de nacimiento los ángeles que vienen a acompañarte en esta vida. Podes conocer cual es tu ángel de la guarda cabalístico, tu ángel solar, tu ángel lunar y tu ángel planetario entre otros. <br>
+                                        La carta se envía por e-mail y se complementa con una lectura  via skype, zoom o videollamada donde te cuento sobre cada ángel, como pueden asistirte en este camino e incluso que mensajes tienen para vos en este momento.
+                                        No te pierdas la oportunidad de recibir esta valiosa información para tu crecimiento espiritual.</p>
+                                    </div>
+                                    <button class="accordion"><p>Tarot angelical</p></button>
+                                    <div>
+                                        <p>
+                                        El tarot angelical es una herramienta de conexión con los ángeles donde podemos recibir mediante las cartas un panorama de las energías que nos acompañan.Por medio de una Lectura Angelical  podrás recibir mensajes claros de Reino Angelico y demás Seres de Luz como una Guía Divina para todos los aspectos de tu vida que desees sanar, aclarar o profundizar.
+                                        </p>
+                                    </div>
+                                    <button class="accordion"><p>Registros akashicos</p></button>
+                                    <div>
+                                        <p>
+                                        Los Registros Akashicos son un espacio multidimensional en el cual se graban o registran todos los eventos, situaciones, pensamientos, emociones y acciones de un ser incluyendo todos los conocimientos y las experiencias de las vidas pasadas, la vida presente y las potencialidades futuras. <br>
+                                        Este sistema energético contiene todas las potencialidades que el Alma posee para su evolución en esta vida y su verdadera razón de ser y el sentido de la existencia.
+                                        </p>
+                                        <p>Lectura de Registros Akashicos: <br>
+                                            Cuando abrimos nuestros Registros Akashicos recibimos canalizaciones que ayudan a darle luz a nuestras dudas y responder nuestras preguntas. <br>
+                                            Una lectura jamás nos dará una orden, ni hablará de DESTINO sino que la información que se recibe nos brindará orientación para tomar las mejores decisiones que nos ayuden a crear y manifestar nuestro camino evolutivo desde nuestro LIBRE ALBEDRÍO. <br>
+                                            Quien consulta sus Registros recibirá solo aquello que esté preparado para escuchar de acuerdo a dónde se encuentre actualmente en términos evolutivos. Ni más, ni menos. <br>
+                                            La información recibida siempre es útil, sutil, amorosa y sobre todas las cosas, sanadora. <br>
+                                            Ya con el simple hecho de abrir los Registros Akashicos estamos comenzando un proceso de sanación profundo. <br> El único requisito para tener una consulta es abrir el corazón.
+                                        </p>
+                                    </div>
+                                    <button class="accordion"><p>Cursos y Talleres</p></button>
+                                    <div>
+                                        <p>
+                                        Encuentros quincenales donde se abre la posibilidad de conectar con los ángeles, aprender sobre sus atributos y funciones. La idea es que obtengas herramientas para invitarlos a ser parte de tu vida. <br> Con cada taller se abren campos de consciencia angelica para tu evolución espiritual
+                                    </p>
+                                </div>                                   
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+    </section>-->
+    <!--aprender a volar
+    <section id="aprender">
+        <br>
+        <div class="main-banner bg-dark-color">
+            <div class="d-table">
+                <div class="d-table-cell">
+                    <div class="container">
+                        <div class="row align-items-center">
+                            <div class="col-lg-6 col-md-12">
+                                <div class="contact-info white-color">
+                                    <h3>Proyecto | Aprender A Volar</h3>
+                                    <ul class="contact-info-list" id="pluma">
+                                        <li>
+                                            Todos los domingos (12:00 PM) Argentina.
+                                        </li>
+                                        <li>
+                                            Programa-Bitácora angelical.
+                                        </li>
+                                        <li>
+                                            Canal: Aprender a Volar Patricia La Rosa. <a href="https://www.youtube.com/channel/UCjfEBELHmPmjN5EWoU_KrWw" target="_blank" style="text-decoration: underline;">ir</a>
+                                        </li>
+                                        <li>
+                                            Todos los domingos te espero para compartir una hora angelical, vamos a explorar información, charlas, meditaciones, mensajes de los ángeles, decretos y mucho más.
+                                        </li>
+                                        <li>
+                                            Te invito a seguir mi canal de youtube y el de aprender a volar para acceder a los programas de todos los terapeutas y el contenido que se va actualizando periódicamente.
+                                        </li>
+                                        <li>
+                                            
+                                        </li>
+                                    </ul>
+                                </div>                                
+                            </div>
+    
+                            <div class="col-lg-6 col-md-12">
+                                <div class="contact-info white-color">
+                                    <h3 style="text-align: center;">Programa del 23/8/2020</h3>
+                                    <h3 style="text-align: center;">Estamos en vivo!!!</h3>
+                                    <div class="video-container">
+                                        <iframe width="560" height="315" src="https://www.youtube.com/embed/C8slSsVmY3M" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>-->
+<?php include("./layout/contacto.php") ?>
+<button onclick="topFunction()" id="myBtn" title="Go to top"><svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-arrow-up-square-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 11.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+  </svg></button>
+<?php include("./layout/script.php") ?>
+</body>
+</html> 
