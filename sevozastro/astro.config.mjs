5f226db940f5102adb1a9029db928df69b59527a
Matias Slimmens <matias-slimmens@hotmail.com>
2026-04-19 16:51:36 -0300

    configuracion github

--- sevozastro/astro.config.mjs
+++ sevozastro/astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+const repository = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
+const isUserOrOrgPagesRepo = repository.endsWith('.github.io');
+const computedBase = isUserOrOrgPagesRepo || repository.length === 0 ? '/' : `/${repository}/`;
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://mslimmens.github.io',
+	base: computedBase
+});
