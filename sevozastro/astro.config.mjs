dc7958aa4f21b7de9ab5c9e2c762c6f67416f354
Matias Slimmens <65687933+mslimmens@users.noreply.github.com>
2026-04-19 17:07:51 -0300

    Update astro.config.mjs
    probando url personal

--- sevozastro/astro.config.mjs
+++ sevozastro/astro.config.mjs
@@ -1,12 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+//import { defineConfig } from 'astro/config';
 
-const repository = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
-const isUserOrOrgPagesRepo = repository.endsWith('.github.io');
-const computedBase = isUserOrOrgPagesRepo || repository.length === 0 ? '/' : `/${repository}/`;
+//const repository = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
+//const isUserOrOrgPagesRepo = repository.endsWith('.github.io');
+//const computedBase = isUserOrOrgPagesRepo || repository.length === 0 ? '/' : `/${repository}/`;
 
 // https://astro.build/config
+//export default defineConfig({
+//	site: 'https://mslimmens.github.io',
+//	base: computedBase
+//});
+
+import { defineConfig } from 'astro/config';
+
 export default defineConfig({
-	site: 'https://mslimmens.github.io',
-	base: computedBase
+    // 1. Cambia 'site' a tu nuevo dominio personalizado
+    site: 'https://matiasslimmens.com.ar',
+    
+    // 2. Define el 'base' manualmente para que coincida con tu directorio
+    base: '/sevozastro',
 });
